79dc6130203a8cdb921e95bbb7550961b95e0364
Barry Schwartz <github@crudfactory.com>
2012-08-27 11:13:12 -0500

    Implemented the "patch" tests in Python. Made installcheck work.

--- tests/patch_test.py
+++ tests/patch_test.py
@@ -1,3 +1,50 @@
 import uninameslookup
-# FIXME: NOT YET WRITTEN.
-exit(0)
+import random, string, sys
+
+def make_patch(db, codepoint):
+    name = db.name(codepoint)
+    annot = db.annot(codepoint)
+    patch = None
+    if name is not None and annot is not None:
+        patch = "{:04X}\t{}\n{}".format(codepoint, name, annot)
+    return patch
+
+db_file = sys.argv[1]
+nameslist_file = sys.argv[2]
+random_codepoint_count = int(sys.argv[3])
+
+random.seed()
+
+db = uninameslookup.uninameslookup(db_file)
+nameslist = file(nameslist_file).read()
+
+failure_count = 0
+
+# Test the codepoints up to 0x10FFFF.
+codepoint = 0
+while codepoint <= 0x10FFFF:
+    #    print codepoint
+    patch = make_patch(db, codepoint)
+    if patch is not None:
+        if nameslist.find(patch) < 0:
+            failure_count += 1
+    codepoint += 1
+
+# Test random codepoints.
+i = 0
+while i <= random_codepoint_count:
+    codepoint = random.randint(0, 0xffffffff)
+    #print codepoint
+    patch = make_patch(db, codepoint)
+    if patch is not None:
+        if nameslist.find(patch) < 0:
+            failure_count += 1
+    i += 1
+
+print("failure_count = " + str(failure_count))
+
+exit_code = 0
+if failure_count != 0:
+    exit_code = 1
+
+exit(exit_code)
